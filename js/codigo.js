56679f24d5373fa36c08f10bb0f2003f4f066bfa
Briant Carrillo <briantc03@gmail.com>
2023-04-06 17:56:48 -0400

    agregamso cambios

---
+++ js/codigo.js
@@ -0,0 +1,14 @@
+
+const paraSegunda = document.getElementById('para-segunda')
+
+function playgame(){
+    paraSegunda.style.display = 'none'
+}
+
+
+
+
+
+
+
+window.addEventListener('load', playgame)
